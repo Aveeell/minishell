4c008bbe065109e8a676bd0a0f9864fe28d284a9
Jimmie Errok <jerrok@mi-o5.msk.21-school.ru>
2022-04-19 12:32:25 +0300

    echo with -n, pwd, cd don't work with space in dirname

--- programs.c
+++ programs.c
@@ -1,26 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   echo.c                                             :+:      :+:    :+:   */
+/*   programs.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jerrok <jerrok@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 10:45:25 by jerrok            #+#    #+#             */
-/*   Updated: 2022/04/19 10:57:55 by jerrok           ###   ########.fr       */
+/*   Updated: 2022/04/19 12:12:09 by jerrok           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	pwd(void)
+{
+	char *pwd;
+
+	pwd = malloc(1024);
+	getcwd(pwd, 1024);
+	printf("%s\n", pwd);
+	free(pwd);
+}
+
 void	echo(char **str)
 {
 	int	i;
-
+	int	flag;
+	
 	i = 1;
+	flag = ft_strcmp(str[1], "-n");
+	if (flag == 0)
+		i = 2;
 	while (str[i])
 	{
-		if (!str[i + 1])
+		if (!str[i + 1] && flag != 0)
 			printf("%s\n", str[i++]);
+		else if (!str[i + 1] && flag == 0)
+			printf("%s", str[i++]);
 		else
 			printf("%s ", str[i++]);
 	}
