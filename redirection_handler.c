2373d4ef3abc983fb9017d124ca1706d60b4f6e6
Mireille Koch <mkoch@at-o7.msk.21-school.ru>
2022-04-29 13:56:45 +0300

    delete unused variables

--- redirection_handler.c
+++ redirection_handler.c
@@ -1,14 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   redirection_handler.c                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jerrok <jerrok@student.21-school.ru>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/25 01:03:31 by iidkhebb          #+#    #+#             */
-/*   Updated: 2022/04/29 10:57:41 by jerrok           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "minishell.h"
 
@@ -21,25 +10,26 @@ int	heredoc(t_command *command, t_envlist *lst)
 	while (1)
 	{
 		prompt = readline("\x1b[34mheredoc > \x1b[37m"); //ждем ввода
-		if (!prompt || !ft_strcmp(command->files->file, prompt)) //если ввод пустой
-			return (free(prompt), close(fd[1]), fd[0]);
+		if (!prompt || !ft_strcmp(command->files->file, prompt)) //если ввод пустой или ввод равен command->files->file
+			return (free(prompt), close(fd[1]), fd[0]); // return(fd[0])
 		ft_putstr_fd(__get_env(prompt, lst, 0), fd[1]);
 		ft_putstr_fd("\n", fd[1]);
 	}
 }
 
-static void	right_redirection(t_command *command, int file_out)
+static void	right_redirection(t_command *command, int file_out) 
+//file_out - файловый дескриптор файла, в который идет вывод
 {
 	if (!ft_strcmp(command->redirection, ">"))
 	{
-		file_out = open(command->files->file, O_CREAT | O_RDWR | O_TRUNC, 0666);
-		dup2(file_out, 1);
-		close(file_out);
+		file_out = open(command->files->file, O_CREAT | O_RDWR | O_TRUNC, 0666); // проверить возвращаемые значения
+		// открываем файл в который будет вестись запись
+		dup2(file_out, 1);  // int dup2(int oldfd, int newfd) - заменяем вывод в файл в стандартный вывод
+		close(file_out);	// закрываем файл для записи
 	}
 	else if (!ft_strcmp(command->redirection, ">>"))
 	{
-		file_out = open(command->files->file, O_CREAT \
-		| O_RDWR | O_APPEND, 0666);
+		file_out = open(command->files->file, O_CREAT | O_RDWR | O_APPEND, 0666); 	// проверить возвращаемые значения, то же самое, что и выше
 		dup2(file_out, 1);
 		close(file_out);
 	}
@@ -50,18 +40,19 @@ static void	left_redirection(t_command *command, int file_out)
 	if (!ft_strcmp(command->redirection, "<"))
 	{
 		file_out = open(command->files->file, O_RDONLY);
-		if (file_out == -1)
+		// открываем файл для чтения
+		if (file_out == -1) // если не открылся, выводим ошибку
 		{
 			printf("%s: No such file or directory\n", command->files->file);
 			exit(1);
 		}
-		dup2(file_out, 0);
-		close(file_out);
+		dup2(file_out, 0);   // int dup2(int oldfd, int newfd) - заменяем ввод из файла на стандартный ввод
+		close(file_out);	 // закрываем файл для чтения
 	}
 	else if (!ft_strcmp(command->redirection, "<<"))
 	{
-		dup2(command->heredoc, 0);
-		close(command->heredoc);
+		dup2(command->heredoc, 0); // int dup2(int oldfd, int newfd) - заменяем ввод из heredoc(?) на стандартный ввод
+		close(command->heredoc);	// закрываем heredoc
 	}
 }
 
@@ -69,12 +60,12 @@ void	redirection_handler(t_command *command, t_envlist *lst)
 {
 	int	file_out;
 
-	file_out = 0;
-	if (command->files != NULL)
+	file_out = 0; 
+	if (command->files != NULL) // идем по списку файлов при редиректах
 	{
-		right_redirection(command, file_out);
-		left_redirection(command, file_out);
-		command->files = command->files->next;
-		redirection_handler(command, lst);
+		right_redirection(command, file_out); 	// обработка > и >>
+		left_redirection(command, file_out);	// обработка < и <<
+		command->files = command->files->next;  // переходим на следующий файл
+		redirection_handler(command, lst); 		// рекурсия
 	}
 }
